5db054cb2374466f48853e1630eb9132b66716c4
JImmyJImmy0 <jerickson317@gmail.com>
2021-06-28 12:28:57 -0600

    make getCardValue function

--- js/app.js
+++ js/app.js
@@ -45,3 +45,28 @@ function shuffleDeck(deck) {
     let randomIndex = Math.floor(Math.random() * deck.length);
     return randomIndex;
 }
+
+function getCardValue(card) {
+    switch (card.value) {
+        case 'A':
+            return 1;
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+        default:
+            return 10;
+    }
+}
