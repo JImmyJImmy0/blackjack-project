46942f176d319bb1cfb5a38f9d6e3882e96600f6
JImmyJImmy0 <jerickson317@gmail.com>
2021-06-28 10:33:52 -0600

    add references for cash, msg, bet

--- js/app.js
+++ js/app.js
@@ -9,7 +9,10 @@
 /*--------------- Cached Element References ---------------*/
 const hitBtn = document.querySelector('#hit-btn');
 const standBtn = document.querySelector('#stand-btn');
-const playBtn = document.querySelector('#play-button')
+const playBtn = document.querySelector('#play-button');
+const cashTotal = document.querySelector('#cash-total');
+const message = document.querySelector('#message');
+const betInput = document.querySelector('#bet');
 
 /*-------------------- Event Listeners --------------------*/
 hitBtn.addEventListener('click', log);
