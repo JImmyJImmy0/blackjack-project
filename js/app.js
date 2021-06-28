289f7abb8fd868751aaff8c4b1ff61102a214257
JImmyJImmy0 <jerickson317@gmail.com>
2021-06-28 10:51:48 -0600

    declare suits and values variables

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@
 
 
 /*----------------------- Variables -----------------------*/
-
+let suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+let values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 
 /*--------------- Cached Element References ---------------*/
