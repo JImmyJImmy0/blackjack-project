7807e0696ddea896934cf3d543841f667f56abd0
JImmyJImmy0 <jerickson317@gmail.com>
2021-06-28 10:21:22 -0600

    add evt listener to stand btn

--- js/app.js
+++ js/app.js
@@ -8,13 +8,19 @@
 
 /*--------------- Cached Element References ---------------*/
 const hitBtn = document.querySelector('#hit-btn');
-
+const standBtn = document.querySelector('#stand-btn');
 
 /*-------------------- Event Listeners --------------------*/
-hitBtn.addEventListener('click', log)
+hitBtn.addEventListener('click', log);
+standBtn.addEventListener('click', log2);
 
 
 /*----------------------- Functions -----------------------*/
 function log() {
     console.log('hit working')
 }
+
+function log2() {
+    console.log('stand working')
+}
+
