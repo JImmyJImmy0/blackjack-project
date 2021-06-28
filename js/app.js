fd68c0e3b9612257f5efdd13395009345498c6e2
JImmyJImmy0 <jerickson317@gmail.com>
2021-06-28 10:19:30 -0600

    add evt listener to hit

--- js/app.js
+++ js/app.js
@@ -7,11 +7,14 @@
 
 
 /*--------------- Cached Element References ---------------*/
-
+const hitBtn = document.querySelector('#hit-btn');
 
 
 /*-------------------- Event Listeners --------------------*/
-
+hitBtn.addEventListener('click', log)
 
 
 /*----------------------- Functions -----------------------*/
+function log() {
+    console.log('hit working')
+}
