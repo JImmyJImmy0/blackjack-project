c4249335b6bbf4615f7f05e6cc2db55cdb8cfe90
JImmyJImmy0 <jerickson317@gmail.com>
2021-06-30 13:38:46 -0600

    update shuffleDeck

--- js/app.js
+++ js/app.js
@@ -67,7 +67,7 @@ function generateDeck() {
 }
 
 // https://devdojo.com/devdojo/create-a-deck-of-cards-in-javascript
-function shuffle(o) {
+function shuffleDeck(o) {
 	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 	return o;
 };
